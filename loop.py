d938e8b517fae76d8ee517e23c7d755309917b49
Harshit Yadav <153003654+harshit0019@users.noreply.github.com>
2025-02-10 16:51:00 +0530

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,15 @@
+#for loop
+for i in range(10):
+  if(i == 5):
+     break # Exit the loop right now
+  print(i)
+for i in range(10):
+  if(i == 5):
+   continue 
+  print(i)
+
+#while loop
+i = 1
+while(i<6):    
+  print(i)
+  i +=1
