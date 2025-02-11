c396a0d1a9b39056c635afc21075e94dcc375c2c
Harshit Yadav <153003654+harshit0019@users.noreply.github.com>
2025-02-11 11:26:38 +0530

    Create loops practise.py

---
+++ loops practise.py
@@ -0,0 +1,20 @@
+#practise question 1
+
+l = ["Harry", "Soham", "Sachin" ," Rahul " ]
+for name in l:
+   if(name.startswith( "S" ) ) :
+     print(f"Hello  {name}")
+
+#practise question 2 
+
+n =  int(input("enter"))
+for i in range(1, 11):
+  print(f"{n} x {i} = {n*i}")
+
+#practise question 3
+n =  int(input("enter"))
+i = 1 
+while(i<11):
+   print(f"{n} x {i} = {n*i}")
+   i +=1
+   
