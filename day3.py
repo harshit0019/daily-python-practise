0f2bb446e3863a54e773986db26f7605ab15f3c2
Harshit Yadav <153003654+harshit0019@users.noreply.github.com>
2025-02-16 20:43:16 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -18,3 +18,30 @@ def printl(list):
     for item in list:
         print(item, end=" ")
 printl(hero)
+
+#2 
+def checkl():
+    word = "am"
+    with open("practise/practise.text", "r") as f:
+        data = f.read()
+        if word in data:
+            print("found")
+        else:
+            print("not found")
+
+def checkf():
+    word = "am"
+    data = True
+    line_no = 1
+    with open("practise/practise.text", "r") as f:
+        while data:
+            data = f.readline()
+            if word in data:
+                print(line_no)
+                return  # Exits the function once the word is found
+            line_no += 1
+    return -1  # Only reached if the word is not found after reading the whole file
+
+result = checkf()
+print(result)  # To see the result of the function
+
