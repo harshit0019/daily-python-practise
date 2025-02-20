326b7a10cf1ed3c94c5022f535a3e1ad4cc6ab9c
Harshit Yadav <153003654+harshit0019@users.noreply.github.com>
2025-02-20 16:04:58 +0530

    Create oops2.py

---
+++ oops2.py
@@ -0,0 +1,34 @@
+#1
+class student:
+    def __init__(self, name):
+        self.name = name
+s1 = student("ram")   
+del s1
+print(s1.name)   
+
+#2
+class car:
+    @staticmethod
+    def start():
+        print("car started")
+class toyata(car):
+    def __init__(self,name):
+        self.name = name
+car1 = toyata("fortuner")
+car2 = toyata("supra")
+
+print(car1.start())
+#3
+class car:
+    def __init__(self,type):
+        self.type = type
+    @staticmethod
+    def start():
+        print("car started")
+class toyata(car):
+    def __init__(self,name ,type):
+        super().__init__(type)
+        self.name = name
+        super().start()
+car1 = toyata("fortuner","ele")
+print(car1.type)
